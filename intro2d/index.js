37c5abd926eae7726ecd78ce0c50172e0b0720b1
TheEndurance <rawa.jalal@gmail.com>
2018-07-08 21:25:42 -0400

    vshader fshader

--- intro2d/index.js
+++ intro2d/index.js
@@ -1,13 +1,36 @@
+var VSHADER_SOURCE = 
+`
+void main() {
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_PointSize = 10.0;
+}`;
+
+var FSHADER_SOURCE = 
+`
+void main(){
+    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+}
+`;
+
+
 function main() {
     var canvas = document.getElementById("canvas");
-    if (!canvas) {
-        console.log("failed to get canvas");
-        return false;
-    }
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle='rgba(0,0,255,1.0)';
 
-    ctx.fillRect(120,10,150,150);
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log("failed to get canvas");
+        return;
+    }
+
+    if(!initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE)){
+        console.log("failed to initialize shaders");
+        return;
+    }
+    
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
 
 document.addEventListener('DOMContentLoaded',()=> main(),false);
